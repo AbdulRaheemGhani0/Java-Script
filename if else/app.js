a516cb4055792242b5bfc718130bdda54ed82bd3
abdulraheemghani <abdulraheemghani0@gmail.com>
2024-03-03 11:50:36 -0800

    if else

---
+++ if else/app.js
@@ -0,0 +1,155 @@
+//var num1 = +prompt('enter a number');
+
+
+
+// Question No.01
+// 1. Write a program to check whether the given input number is
+// divisible by 3 or else show a message “Number is not divisible
+// by 3”.
+// Answer.
+
+// if (num1%3===0 ) {
+//     console.log('number is divible by 3' )
+//     }
+      
+// else {
+//     console.log('else  vali condition')
+// }
+
+
+
+
+
+
+
+
+
+//Question No.01
+// 2. Write a program that checks whether the given input is an even
+// number or an odd number.
+//Answer
+
+//  if (num1%2===0 ) {
+//     console.log('Even number' )
+//     }
+      
+//  else  {
+//      console.log('Odd number')
+//  }
+
+
+
+
+
+//Question No.03
+// 3. Write an if/else statement with the following condition:
+// If the variable age is greater than 18, output "Old enough",
+// otherwise output "Too young".
+
+// Answer
+
+//   if (num1>18 ) {
+//      console.log('Old enough' )
+//      }
+      
+//  else if (num1<18 ) {
+//       console.log('Too young')
+//   }
+
+//   else  {
+//     console.log('not a number')
+// }
+
+
+//var userName=prompt("Enter your name")
+
+//Question No.04
+// 4. Write a program that prompts the user for their name, and then
+// displays a special greeting to that person if their name is the
+// same as yours. If the name entered by the user is anything
+// other than your name, your code should not produce any
+// output.
+
+//Answer
+
+// if (userName === "abdulraheem") {
+//     document.write("Hi! Most Wellcome")
+// }
+// else if (userName !== "abdulraheem"){
+//     document.write("Sorry")
+// }
+
+// else {
+//     document.write("not a number")
+// }
+ //Question no.05
+//5. Write a program to check whether the given input number is
+// divisible by 3 or not by using Switch Case statements. Show a
+// message “Number is not divisible by 3” or “Number is divisible
+// by 3”.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//else if (num1 < 10) {
+ ////   console.log('else if vali condition')
+//}
